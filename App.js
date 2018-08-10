2574fd6c865ceba1460a4f20d52db6f2986c544a
Alissa Niewiadomski <alissaaadomski@gmail.com>
2018-08-10 12:58:01 -0700

    basic contact list

--- App.js
+++ App.js
@@ -1,38 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
-  Text,
-  View
+  View,
+  Button,
+  FlatList,
+  Text
 } from 'react-native';
+import Contacts from 'react-native-contacts';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-type Props = {};
-export default class App extends Component<Props> {
+    this.state = {
+      contacts: [],
+    };
+  };
+
+  getContacts = () => {
+    Contacts.getAll((err, c) => {
+      if (err) {
+        throw err;
+      }
+      this.setState({contacts: c});
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button title={"Get Contacts"} onPress={() => this.getContacts()} />
+        {
+          this.state.contacts.length !== 0
+          ? <FlatList
+              data={this.state.contacts}
+              renderItem={(contact) => <Text>{contact.item.givenName}</Text>}
+            />
+          : null
+        }
       </View>
     );
   }
