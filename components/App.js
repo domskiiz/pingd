9e2020c94f8b061539902dce908b4fc223777b64
Alissa Niewiadomski <alissaaadomski@gmail.com>
2018-08-25 12:03:37 -0700

    added linter file and 4 spaces in app js (fk u ian)

--- components/App.js
+++ components/App.js
@@ -1,65 +1,67 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  StyleSheet,
-  View,
-  Button,
-  FlatList,
-  Text
+    StyleSheet,
+    View,
+    Button,
+    FlatList,
+    Text,
 } from 'react-native';
 import Contacts from 'react-native-contacts';
-import detectFirstLaunch from "../utils/detectFirstLaunch";
+// import detectFirstLaunch from "../utils/detectFirstLaunch";
 
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      contacts: [],
+        this.state = {
+          contacts: [],
+        };
+    }
+
+    getContacts = () => {
+        Contacts.getAll((err, c) => {
+            if (err) {
+                throw err;
+            }
+            this.setState({contacts: c});
+        });
     };
-  };
 
-  getContacts = () => {
-    Contacts.getAll((err, c) => {
-      if (err) {
-        throw err;
-      }
-      this.setState({contacts: c});
-    });
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Button title={"Get Contacts"} onPress={() => this.getContacts()} />
-        {
-          this.state.contacts.length !== 0
-          ? <FlatList
-              data={this.state.contacts}
-              renderItem={(contact) => <Text>{contact.item.givenName}</Text>}
-            />
-          : null
-        }
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button title={'Get Contacts'}
+                        onPress={() => this.getContacts()}
+                />
+                    {
+                      this.state.contacts.length !== 0
+                      ? <FlatList
+                          data={this.state.contacts}
+                          renderItem={(c) => <Text>{c.item.givenName}</Text>}
+                        />
+                      : null
+                    }
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
