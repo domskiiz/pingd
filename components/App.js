dfd5a0b505e067e5925d8be53488d22888e147ae
Ian Brault <ian.brault@engineering.ucla.edu>
2018-08-25 17:28:18 -0700

    Add keys to FlatList.

--- components/App.js
+++ components/App.js
@@ -55,6 +55,7 @@ export default class App extends Component {
             contactList = (
                 <FlatList
                     data={this.state.contacts}
+                    keyExtractor={(item, index) => index.toString()}
                     renderItem={this.renderContactCard}
                 />
             );
