cc97c70ec49a6100155c3dbb410a665fc0d6617f
Ian Brault <ian.brault@engineering.ucla.edu>
2018-08-25 20:38:10 -0700

    Add in-progress phone number formatting function, needs to be tested with actual contacts.

--- components/ContactCard/ContactCard.js
+++ components/ContactCard/ContactCard.js
@@ -37,10 +37,36 @@ class ContactCard extends Component {
         this.props.addContact(contact, priority);
     }
 
+    // TODO: this might not even be needed, but the default phone numbers
+    // TODO: returned by react-native-contacts are in a weird-ass format
+    formatPhoneNumber(phoneNumber) {
+        let digits = '';
+        for (let i = 0; i < phoneNumber.length; i++) {
+            let ch = phoneNumber[i];
+            if (ch >= '0' && ch <= '9')
+                digits += ch;
+        }
+
+        // TODO: this is janky, won't work for longer country codes
+        if (digits.length === 11) {
+            let countryCode = digits[0];
+            let areaCode = digits.slice(1, 4);
+            let number = `${digits.slice(4, 7)}-${digits.slice(7, 11)}`;
+            return `+${countryCode} (${areaCode}) ${number}`;
+        } else if (digits.length === 10) {
+            let areaCode = digits.slice(0, 3);
+            let number = `${digits.slice(3, 6)}-${digits.slice(6, 10)}`;
+            return `(${areaCode}) ${number}`;
+        } else if (digits.length === 7) {
+            return `${digits.slice(0, 3)}-${digits.slice(3, 7)}`;
+        } else {
+            return digits;
+        }
+    }
+
     render() {
         let name = `${this.props.firstName} ${this.props.lastName}`;
-        // TODO: put in a consistent format
-        let phoneNumber = this.props.phoneNumber;
+        let phoneNumber = this.props.phoneNumber; // this.formatPhoneNumber(phoneNumber);
 
         let card = null;
         if (this.state.flipped)
